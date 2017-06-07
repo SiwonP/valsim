7b1391933d2fcc16a79902a64246f3038d35434f
sopiU6 15 <sopiu615@MacBook-Air-de-sopiU6.local>
2017-06-07 11:13:56 +0200

    Some list functions implemented, some are still missing though

--- src/list.c
+++ src/list.c
@@ -1,3 +1,55 @@
+#include <stdlib.h>
 #include "../h/list.h"
 
+//Initialization of the list with a first element containing the any passed in argument of
+//the function
+List *initialisation(void *any) {
+    List *liste = malloc(sizeof(*liste));
+    Element *element = malloc(sizeof(*element));
 
+    if ( liste == NULL ||element == NULL ) {
+        exit(EXIT_FAILURE);
+    }
+
+    element->el = any;
+    element->next = NULL;
+    liste->premier = element;
+
+    return liste;
+}
+
+//Insert at the end of the List liste an element
+//containing any also passed in argument of the function
+void insert_at_the_end(List *liste, void *any) {
+    Element *newEl = malloc(sizeof(*newEl));
+    newEl->el = any;
+
+    if ( liste == NULL ||newEl == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    
+    Element *test = liste->premier;
+
+    while(test->next != NULL) {
+        test = test->next;
+    }
+
+    test->next = newEl;
+}
+
+//Display the Liste liste. Since elements of the Liste
+//are void* types, you should give a function 
+//able to display each element from a certain type 
+//otherwiser the display_list function doesn't know how to 
+//display the elements of its List argument
+void display_list(List *liste, void(*f)(void*)) {
+   if (liste == NULL) {
+      exit(EXIT_FAILURE);
+   }
+
+  Element *current = liste->premier; 
+  while ( current != NULL) {
+      (*f)(current);
+      current = current->next;
+  }
+}
